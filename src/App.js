79a2f8a377591b4c1cdc25c40733ab33aac89d99
Ryan Yang <javathon@gmail.com>
2019-11-12 14:47:40 -0800

    added ziv database

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
-import React, {useState} from 'react';
+import React from 'react';
+import counts from './Ziv.js';
 import './App.css';
 
 function App() {
-    const [zivCounter, setZivCounter] = useState(1);
-
     return (
         <div className="ziv-law--wrapper">
             <div className="ziv-law--title">
                 Number of times Ziv has mentioned Ziv's Law:
             </div>
             <div className="ziv-law--counter">
-                {zivCounter}
+                {counts.length}
             </div>  
         </div>
     );
---
+++ src/Ziv.js
@@ -0,0 +1,4 @@
+const counts = [
+    {time: '2019-11-12T22:39:06.873Z'}
+]
+export default counts;
