af8facaa2dc40a6be8fda58e97b29650d4724c39
Ryan Yang <javathon@gmail.com>
2019-11-15 15:24:05 -0800

    Updates

---
+++ precache-manifest.df185ea328dbb2e418aaad0987bbce11.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82e201c24be65641ac17b94607ed7c95",
+    "url": "/ziv-counter/index.html"
+  },
+  {
+    "revision": "7697c15d8e22e40af3b9",
+    "url": "/ziv-counter/static/css/main.4d734d82.chunk.css"
+  },
+  {
+    "revision": "be22b4d2452b6c4f3de9",
+    "url": "/ziv-counter/static/js/2.7748130f.chunk.js"
+  },
+  {
+    "revision": "7697c15d8e22e40af3b9",
+    "url": "/ziv-counter/static/js/main.35e88d19.chunk.js"
+  },
+  {
+    "revision": "ebad1247d0291074d924",
+    "url": "/ziv-counter/static/js/runtime-main.08eb28c9.js"
+  },
+  {
+    "revision": "4a7a86d23b1353e4f86f2a9e1e665772",
+    "url": "/ziv-counter/static/media/Ziv.4a7a86d2.jpg"
+  },
+  {
+    "revision": "64de38475d6ea6a35c277c6873f7fa08",
+    "url": "/ziv-counter/static/media/history-24px.64de3847.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ziv-counter/precache-manifest.e3b9262cacda123c66f222ce8a81ee3c.js"
+  "/ziv-counter/precache-manifest.df185ea328dbb2e418aaad0987bbce11.js"
 );
 
 self.addEventListener('message', (event) => {
